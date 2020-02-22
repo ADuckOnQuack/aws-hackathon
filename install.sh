025ce626e99b14abffa57f797ff6409ea86dccf0
Brett Evrist <brettevrist10@gmail.com>
2020-02-22 10:14:12 +0000

    moved network to host

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@ cd ../backend
 cp Dockerfile-backend Dockerfile
 sudo docker build -t node-backend .
 
-BACKEND_ID=$(sudo docker run -d --net=host --rm node-backend)
-BACKEND_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $BACKEND_ID)
+sudo docker run -d --net=host --rm node-backend
+docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $BACKEND_ID
 
 cd ../aws-hackathon
 # sed -i "s/http:\/\/localhost:9000\/testAPI/http:\/\/$BACKEND_IP:9000\/testAPI/g" aws-app/src/containers/Test.js
@@ -17,7 +17,7 @@ cd ../aws-hackathon
 cp Dockerfile-front Dockerfile
 sudo docker build -t node-front .
 
-sudo docker run -d --rm -p:80:5000 node-front
+sudo docker run -d --net=host --rm -p:80:5000 node-front
 
 
 
