8cafe6f2928fa313d7a30a350474e8410e1919e9
Brett Evrist <brettevrist10@gmail.com>
2020-02-22 08:44:09 +0000

    fixed .gitignore

--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@ rm -rf aws-app/node_modules
 cp -R ./ ../backend
 
 cp Dockerfile-front Dockerfile
-docker build -t node-front .
+sudo docker build -t node-front .
 
 cd ../backend
 cp Dockerfile-backend Dockerfile
-docker build -t node-backend .
+sudo docker build -t node-backend .
 
-docker run -d --rm node-backend
-docker run -d --rm -p:3000:80 node-front
+sudo docker run -d --rm node-backend
+sudo docker run -d --rm -p:3000:80 node-front
