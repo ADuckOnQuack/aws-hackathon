700c1a663bb841d473dd621c2d52f1c5706d6866
Brett Evrist <brettevrist10@gmail.com>
2020-02-22 11:01:01 +0000

    giving up

--- install.sh
+++ install.sh
@@ -8,13 +8,13 @@ cd ../backend
 cp Dockerfile-backend Dockerfile
 sudo docker build -t node-backend .
 
-BACKEND_ID=$(sudo docker run -d --rm node-backend)
+BACKEND_ID=$(sudo docker run -d --net=host --rm node-backend)
 BACKEND_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $BACKEND_ID)
 
 cd ../aws-hackathon
-sed -i "s/http:\/\/localhost:9000\/testAPI/http:\/\/$BACKEND_IP:9000\/testAPI/g" aws-app/src/containers/Test.js
+# sed -i "s/http:\/\/localhost:9000\/testAPI/http:\/\/$BACKEND_IP:9000\/testAPI/g" aws-app/src/containers/Test.js
 
 cp Dockerfile-front Dockerfile
 sudo docker build -t node-front .
 
-sudo docker run -d --rm -p:80:5000 node-front
+sudo docker run -d --rm --net=host -p:80:5000 node-front
