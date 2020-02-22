f0b307c82ef615dddef8f6cd60521996381346c7
Bobby Laudeman <bobbylaudeman@mbpro.local>
2020-02-22 06:16:26 -0800

    get users

--- aws-app/api/routes/classes.js
+++ aws-app/api/routes/classes.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var connection = require('../db')
 
 /* GET classes page. */
 router.get('/?', function(req, res, next) {
+  console.log(connection.query('SELECT * FROM '));
   res.send('Getting all classes for user:' + req.query.uid);
 });
 
--- aws-app/api/routes/users.js
+++ aws-app/api/routes/users.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var connection = require('../db')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  connection.query('SELECT * FROM users', function (err, result, fields) {
+    if (err) throw err;
+    for (var i in result)
+      console.log('Usernames: ', result[i].username);  
+    });
   res.send('respond with a resource');
 });
 
