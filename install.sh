450e27556c649779f0515456132e0048f1a9cf99
Brett Evrist <brettevrist10@gmail.com>
2020-02-22 10:08:23 +0000

    pls fix 4 realz

--- install.sh
+++ install.sh
@@ -8,11 +8,11 @@ cd ../backend
 cp Dockerfile-backend Dockerfile
 sudo docker build -t node-backend .
 
-BACKEND_ID=$(sudo docker run -d --rm node-backend)
+BACKEND_ID=$(sudo docker run -d --net=host --rm node-backend)
 BACKEND_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $BACKEND_ID)
 
 cd ../aws-hackathon
-sed -i "s/http:\/\/localhost:9000\/testAPI/http:\/\/$BACKEND_IP:9000\/testAPI/g" aws-app/src/containers/Test.js
+# sed -i "s/http:\/\/localhost:9000\/testAPI/http:\/\/$BACKEND_IP:9000\/testAPI/g" aws-app/src/containers/Test.js
 
 cp Dockerfile-front Dockerfile
 sudo docker build -t node-front .
