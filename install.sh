231b4ac0808ff7c696453bfb6cfb924ffeb7bc37
Brett Evrist <brettevrist10@gmail.com>
2020-02-22 09:53:59 +0000

    point frontend server to backend

--- install.sh
+++ install.sh
@@ -4,12 +4,21 @@ rm -rf aws-app/build
 rm -rf aws-app/node_modules
 cp -R ./ ../backend
 
-cp Dockerfile-front Dockerfile
-sudo docker build -t node-front .
-
 cd ../backend
 cp Dockerfile-backend Dockerfile
 sudo docker build -t node-backend .
 
-sudo docker run -d --rm node-backend
+BACKEND_ID=$(sudo docker run -d --rm node-backend)
+BACKEND_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $BACKEND_ID)
+
+cd ../aws-hackathon
+sed -i "s/http:\/\/localhost:9000\/testAPI/http:\/\/$BACKEND_IP:9000\/testAPI/g" aws-app/src/containers/Test.js
+
+cp Dockerfile-front Dockerfile
+sudo docker build -t node-front .
+
 sudo docker run -d --rm -p:80:5000 node-front
+
+
+
+sed -i "s/http:\/\/localhost:9000\/testAPI/http:\/\/aaaaa:9000\/testAPI/g" aws-hackathon/aws-app/src/containers/Test.js
